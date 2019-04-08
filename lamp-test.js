78b7de2fc854091038bd055d4bd8e0c7b746f5c3
Aramael Pena-Alcantara <aramael@pena-alcantara.com>
2019-04-07 23:27:53 -0400

    Added Global Options with config.json

--- lamp-test.js
+++ lamp-test.js
@@ -1,13 +1,67 @@
-var osc = require("osc");
-var game = require('./game.js');
+// import modules
 const delay = require('delay');
+const fs = require('fs');
+var osc = require("osc");
+
+// import project
+var game = require('./game.js');
+
+// process arguments
+var args = require('minimist')(process.argv.slice(2));
+
+let clear = false;
+let remoteAddress = undefined;
+let remotePort = undefined;
+
+if (args === Object(args)){
+	if (args.hasOwnProperty('clear') && args.clear !== undefined){
+		if (args.clear){
+			clear = true;
+		}
+	}
+
+	// remote port
+	if (args.hasOwnProperty('remote-port') && args['remote-port'] !== undefined){
+		// set remote option
+		remotePort = args['remote-port'];
+	} else {
+		// load in remote options from disk
+		let diskConfig = JSON.parse(fs.readFileSync('config.json'));
+		remotePort = diskConfig.osc.remotePort;
+	}
+
+	// remote address
+	if (args.hasOwnProperty('remote-address') && args['remote-address'] !== undefined){
+		// set remote option
+		remoteAddress = args['remote-address'];
+	} else {
+		// load in remote options from disk
+		let diskConfig = JSON.parse(fs.readFileSync('config.json'));
+		remoteAddress = diskConfig.osc.remoteAddress;
+	}
+
+	// save configured options
+	if (args.hasOwnProperty('save') && args.save){
+		// save remote to disk
+		// load in remote options from disk
+		let diskConfig = JSON.parse(fs.readFileSync('config.json'));
+
+		// write to disk
+		diskConfig.osc.remoteAddress = remoteAddress;
+		diskConfig.osc.remotePort = remotePort;
+		fs.writeFileSync('config.json', JSON.stringify(diskConfig));
+	}
+} else {
+	console.error('something unexpected happened while parsing the command line arguments.');
+	process.exit();
+}
 
 // Initalise OSC Controller
 var oscController = new osc.UDPPort({
     localAddress: "0.0.0.0",
     localPort: 8000,
     remotePort: 9000,
-    remoteAddress: "192.168.0.8"
+    remoteAddress: remoteAddress
 });
 
 oscController.open();
@@ -18,25 +72,34 @@ oscController.on('ready', function (){
 			console.log(`----> Lamp Test for Bottle ${i}`);
 
 			// Send Empy Bottle
-			// game.sendEmptyBottleStatus(oscController, i);
-			game.clearEmptyBottleStatus(oscController, i);
+			if (!clear){
+				game.sendEmptyBottleStatus(oscController, i);
+			} else {
+				game.clearEmptyBottleStatus(oscController, i);
+			}
 
 			await delay(250);
 
 			// Send Animation Complete
-			// game.sendAnimationCompleteStatus(oscController, i);
-			game.clearAnimationCompleteStatus(oscController, i);
+			if (!clear){
+				game.sendAnimationCompleteStatus(oscController, i);
+			} else {
+				game.clearAnimationCompleteStatus(oscController, i);
+			}
 
 			await delay(250);
 			// Send Animation Name
-			// game.sendName(oscController, i, `Name ${i}`);
-			game.clearName(oscController, i);
+			if (!clear){
+				game.sendName(oscController, i, `Name ${i}`);
+			} else {
+				game.clearName(oscController, i);
+			}
 		}
 	})();
 });
 
 oscController.on("message", function(message, timetag, info) {
-	console.log(message); 
+	console.log(message);
 });
 
 oscController.on("error", function (err) {
